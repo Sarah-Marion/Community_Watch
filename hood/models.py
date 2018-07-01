11bab7ce30b64a9036d509b4ed9d6002b8db7b67
Sarah-Marion <devsarahmarion@gmail.com>
2018-07-01 11:04:53 +0300

    created a a sign up and login form

---
+++ hood/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Hood(models.Model):
+    """
+    Hood class that defines objects of each neighbourhood
+    """
+    hoodName = models.CharField(max_length=100)
+    hoodLocation = models.CharField(max_length=50, null=True)
+    occupantsCount = models.PositiveSmallIntegerField(null=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return self.hoodName
+
+    def save_hood(self):
+        self.save()
+
+    def delete_hood(self):
+        self.delete()
